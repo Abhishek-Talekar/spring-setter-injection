717f4fd8bf006e639a1c9e474fc1fc4e7748f38d
Abhishek Talekar <talekarabhishek02@gmail.com>
2020-02-20 15:18:43 +0530

    Third Commit

--- 2SetterInjection/src/com/si/test/SITest.java
+++ 2SetterInjection/src/com/si/test/SITest.java
@@ -1,5 +1,20 @@
 package com.si.test;
 
-public class SITest {
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
+import com.si.beans.Channel;
+import com.si.beans.SonyChannelImpl;
+import com.si.beans.Television;
+
+public class SITest {
+	public static void main(String[] args) {
+		Channel channel;
+		Television television = null;
+		BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("com/si/common/application-context.xml"));
+		channel = beanFactory.getBean("sony",SonyChannelImpl.class);
+		television.watchTv();
+		
+	}
 }
